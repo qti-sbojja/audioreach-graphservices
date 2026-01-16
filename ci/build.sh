6299c76d43be19b2241c028f270765cff5cd415d
ffrancis <ffrancis@qti.qualcomm.com>
2026-01-16 12:09:17 +0530

    CI: Remove CFLAGS override and update configure header path
    Update the configure command by eliminating the unnecessary CFLAGS
    override to prevent redundant compiler flag suppression.
    
    Signed-off-by: ffrancis <ffrancis@qti.qualcomm.com>

--- ci/build.sh
+++ ci/build.sh
@@ -24,6 +24,6 @@ autoreconf -Wcross --verbose --install --force --exclude=autopoint
 autoconf --force
 
 # Run the configure script with the specified arguments
-./configure CFLAGS="-Wno-int-conversion" ${BUILD_ARGS} --with-sanitized-headers=${GITHUB_WORKSPACE}/install/sysroots/armv8-2a-poky-linux/usr/lib/module/build/include/uapi/sound/qcom
+./configure ${BUILD_ARGS}
 # make
 make DESTDIR=${GITHUB_WORKSPACE}/build install
